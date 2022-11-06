c6e0dcd38b7888133d2cce43d5b4a6637d41fd8f
203452 <203452@ids.upchiapas.edu.mx>
2022-11-06 07:22:10 -0600

    se consumio la api

--- src/components/Compra.jsx
+++ src/components/Compra.jsx
@@ -1,39 +1,52 @@
 import "../assets/styles/compra.css";
-import { useNavigate  } from "react-router-dom";
-import fotogta from "../assets/images/accion/gta.jpeg";
+import { useState, useEffect } from "react";
+
+
 
 function Compra() {
-    const navigate= useNavigate();
-  const accion = {
-    nombre: ["God of War", "GTA V"],
-    precio: ["110", "300", "113"],
-    descripcion: [
-      "Hollow Knight es un videojuego perteneciente al género metroidvania desarrollado y publicado por Team Cherry.",
-      "Grand Theft Auto V es un videojuego de acción-aventura de mundo abierto desarrollado por el estudio Rockstar North y distribuido por Rockstar Games.",
-      "Minecraft es un videojuego de construcción, de tipo «mundo abierto» o sandbox creado originalmente por el sueco Markus Persson, ​ y posteriormente desarrollado por su empresa, Mojang Studios.",
-    ],
-    imagen: [
-      "./src/images/foto1.jpeg",
-      "./src/images/foto2.jpeg",
-      "./src/images/foto3.jpg",
-    ],
-  };
+  const [data, setData] = useState([]);
+  const [nombre, setNombre] =useState('');
+  const [descripcion, setDescripcion] =useState('');
+
+  console.log(data);
+
+  useEffect(() => {
+    const getGames = () => {
+      fetch("http://localhost:3000/api/games")
+        .then((res) => res.json())
+        .then((res) => setData(res))
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getGames();
+  }, []);
+
+  const handleChangeNombre = (e) =>{
+    setNombre(e.target.value)
+    console.log(e.target.value)
+  }
 
   return (
     <div className="caja-compra">
       <div className="container-compra">
         <div className="compra_left">
           <div className="opciones-compra">
-            <select name="" id="">
-              <option value="">{accion.nombre[0]}</option>
-              <option value="">{accion.nombre[1]}</option>
+            <select  value={nombre} onChange={handleChangeNombre}>
+              {
+                data.map(data => (
+                  <option>{data.nombre}</option>
+                ))
+              }
             </select>
           </div>
 
           <div>
             <label htmlFor="">Cantidad:</label>
-            <input type="number" id="cantidad"/>
-            <button id="add" className="boton_compra">Agregar</button>
+            <input type="number" id="cantidad" />
+            <button id="add" className="boton_compra">
+              Agregar
+            </button>
           </div>
 
           <div className="tabla">
@@ -55,15 +68,15 @@ function Compra() {
                 </tr> */}
               <tfoot>
                 <tr>
-                  <th colspan="3">SubTotal: </th>
+                  <th colSpan="3">SubTotal: </th>
                   <th id="sub"></th>
                 </tr>
                 <tr>
-                  <th colspan="3">IVA 16%: </th>
+                  <th colSpan="3">IVA 16%: </th>
                   <th id="iva"></th>
                 </tr>
                 <tr>
-                  <th colspan="3">Total: </th>
+                  <th colSpan="3">Total: </th>
                   <th id="total"></th>
                 </tr>
               </tfoot>
@@ -71,27 +84,37 @@ function Compra() {
           </div>
 
           <div>
-            <button id="buy" className="boton_compra">Comprar</button>
-            <button id="clear" className="boton_compra">Borrar</button>
+            <button id="buy" className="boton_compra">
+              Comprar
+            </button>
+            <button id="clear" className="boton_compra">
+              Borrar
+            </button>
           </div>
 
           <div>
-            <button id="back" className="boton_compra" onClick={() => window.location.reload()}>Back</button>
+            <button
+              id="back"
+              className="boton_compra"
+              onClick={() => window.location.reload()}
+            >
+              Back
+            </button>
           </div>
         </div>
 
         <div className="compra_right">
           <div className="informacion">
             <div id="imagen">
-                <img src={fotogta} alt="" />
+            {/* <img src={image} alt={nombre} /> */}
             </div>
             <div id="description">
-                <h4>Nombre:</h4>
-                gta
-                <h4>Precio:</h4>
-                12
-                <h4>Descripción:</h4>
-                Grand Theft Auto V es un videojuego de acción-aventura de mundo abierto desarrollado por el estudio Rockstar North y distribuido por Rockstar Games.
+              <h4>Nombre:</h4>
+                {nombre}
+              <h4>Precio:</h4>
+          
+              <h4>Descripción:</h4>
+       
             </div>
           </div>
         </div>
